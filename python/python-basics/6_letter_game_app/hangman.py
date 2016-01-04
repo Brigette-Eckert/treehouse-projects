439bc1be2e863faebff4d544a3eeb7cf92e678c1
Brigette Eckert <brigette.eckert@live.com>
2016-01-04 06:34:15 -0800

    finished python basics course

--- python/python-basics/6_letter_game_app/hangman.py
+++ python/python-basics/6_letter_game_app/hangman.py
@@ -1,51 +1,97 @@
+import os
+import sys
 import random
+
 #make a list of words
-words = ["kittens", "icy", "wizard", "angle", "fire", "ocean", "whale", "mellow", "marathon", "cloistered", "ocean", "cry", "werewolf", "medal", "arch", "electricity", "royal", "existence", "nostalgic", "wheel", "rose", "mythology", "rain", "xenophobia", "exuberant", "swift", "laugh", "unicorn", "narwhal", "button", "", "tan", "sugar", "succinct", "available", "love", "candy", "marathon", "quartz", "flapjack", "squid", "cheese", "joule", "galaxy", "jazz", "raspberry", "cat", "fox", "relationship", "species", "bug", "beetle", "space", "juice", "cookies", "autumn", "summer", "solstice", "equinox", "eclipse"]
+words = ["kittens", "icy", "wizard", "angle", "fire", "ocean", "whale", "mellow", "marathon", "cloistered", "ocean", "cry", "werewolf", "medal", "arch", "electricity", "royal", "existence", "nostalgic", "wheel", "rose", "mythology", "rain", "xenophobia", "exuberant", "swift", "laugh", "unicorn", "narwhal", "button", "tan", "sugar", "succinct", "available", "love", "candy", "marathon", "quartz", "flapjack", "squid", "cheese", "joule", "galaxy", "jazz", "raspberry", "cat", "fox", "relationship", "species", "bug", "beetle", "space", "juice", "cookies", "autumn", "summer", "solstice", "equinox", "eclipse"]
 
-while True:
-	start = input("Press enter to start, or press Q to quit ")
-	if start.lower() == "q":
-		break
-	#pick a random word from list
-	secret_word = random.choice(words)
-	bad_guesses = []
-	good_guesses = []
+#clear screen
+def clear():
+	if os.name == 'nt':
+		os.system('cls')
+	else: 
+		os.system('clear')
 
-	while len(bad_guesses) < 7 and len(good_guesses) != len(list(secret_word)):
-		#draw guessed letters, spaces and strikes
-		for letter in secret_word:
-			if letter in good_guesses:
-				print(letter, end='')
-			else:
-				print('_ ' , end='')
+def draw(bad_guesses, good_guesses, secret_word):
+	clear()
+	print('Strikes: {}/7'.format(len(bad_guesses)))
+	print('')
 
-		print('')
-		print('Strikes: {}/7'.format(len(bad_guesses)))
-		print('')
+	for letter in bad_guesses:
+		print(letter, end='')
+	print('\n\n')
 
+	for letter in secret_word:
+		if letter in good_guesses:
+			print(letter, end='')
+		else:
+			print('_ ' , end='')
+
+	print('')
+
+def get_geuss(bad_guesses, good_guesses):
+	while True:
 		#take guess	
 		guess = input("Guess a letter ").lower()
 		
 		if len(guess) !=1:
 			print("You can only guess a single letter")
-			continue
 		elif guess in bad_guesses or guess in good_guesses:
 			print("You've already guessed that letter")
-			continue
 		elif not guess.isalpha():
 			print("You can only guess letters")
-			continue
+		else: 
+			return guess
+def play(done):
+	clear()
+	secret_word = random.choice(words)
+	bad_guesses = []
+	good_guesses = []
+
+	while True:
+		draw(bad_guesses, good_guesses, secret_word)
+		guess = get_geuss(bad_guesses, good_guesses)
 
 		if guess in secret_word:
 			good_guesses.append(guess)
-			print("bad guesses: {}".format(bad_guesses))
-			if len(good_guesses) == len(list(secret_word)):
-				#print out win/lose 
-				print("Congratulations! We have a winner! The word was {}".format(secret_word))
-				break
+			found = True
+			for letter in secret_word:
+				if letter not in good_guesses:
+					found = False
+			if found: 
+				draw(bad_guesses, good_guesses, secret_word)
+				print("Congratulations! You win!")
+				print("The word was {}".format(secret_word))
+				done = True
 		else: 
 			bad_guesses.append(guess)
-			print("bad guesses: {}".format(bad_guesses))
+			if len(bad_guesses) == 7:
+				draw(good_guesses, bad_guesses, secret_word)
+				print("You lose.")
+				print("The word was {}.".format(secret_word))
+				done = True
 
+		if done: 
+			play_again = input("Play again? Y/n ").lower()
+			if play_again != 'n':
+				return play(done=False)
+			else:
+				sys.exit()
+
+def welcome():
+	start = input("Press enter to start or Q to quit ").lower()
+	if start == "q":
+		print("Bye!")
+		sys.exit()
 	else:
-		print("Sorry, you didn't guess it. The word was {}. better luck next time".format(secret_word))
+		return True
+print("Welcome")
+
+done = False
+
+while True:
+	clear()
+	welcome()
+	play(done)
+
+#bugs: prints win message  multlipe times, says you've won after one correct guesses- the word was '.'
