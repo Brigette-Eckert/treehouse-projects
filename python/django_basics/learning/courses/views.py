5c976c4f6f70cfd4f2f820e30294df419d9cf1ef
Brigette Eckert <brigette.eckert@live.com>
2016-06-12 00:15:56 -0700

    finished section 4 of django basics

--- python/django_basics/learning/courses/views.py
+++ python/django_basics/learning/courses/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
 from courses.models import Course
 
@@ -8,3 +6,7 @@ from courses.models import Course
 def course_list(request):
     courses = Course.objects.all()
     return render(request, 'courses/course_list.html', {'courses': courses})
+
+def course_detail(request, pk):
+    course = get_object_or_404(Course, pk=pk)
+    return render(request, 'courses/course_detail.html', {'course': course})
