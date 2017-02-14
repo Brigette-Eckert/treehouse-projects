6d29377a653daee857982c753194b288f48823b3
Brigette Eckert <Brigette.Eckert@live.com>
2017-02-14 13:56:54 -0800

    finished first steps section of python testing course

---
+++ python/testing/1_first_steps_testing/rock_paper_scissors/test.py
@@ -0,0 +1,15 @@
+import unittest
+
+import moves
+
+
+class MoveTests(unittest.TestCase):
+    def test_five_plus_five(self):
+        assert 5 + 5 == 10
+
+    def test_one_plus_one(selfs):
+        assert not 1 + 1 == 3
+
+## allows to run test.py directly rather than python -m unittest testp.py
+if __name__ == '__main__':
+    unittest.main()
