4f0ae6c8fb0eb8138a8735d1c5a9555a82788ec8
Brigette Eckert <Brigette.Eckert@live.com>
2017-02-28 11:11:35 -0800

    finished welcome to flask section of flask basics course

--- python/flask_basics/1_welcome_to_flask/simple_app.py
+++ python/flask_basics/1_welcome_to_flask/simple_app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Hello from Treehouse"
+@app.route('/<name>')
+def index(name="Nikola"):
+    return "Hello {}!".format(name)
+
+
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1, num2):
+    return '{} + {} = {}'.format(num1, num2, num1+num2)
 
 app.run(debug=True, port=8000, host='0.0.0.0')
