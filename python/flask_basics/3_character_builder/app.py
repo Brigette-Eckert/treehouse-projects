5e2c8dbd479289f7286f97e55cec360c3b846744
Brigette Eckert <Brigette.Eckert@live.com>
2017-03-08 10:12:06 -0800

    started character builder section of flask basics course

---
+++ python/flask_basics/3_character_builder/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/save', methods=['POST'])
+def save():
+    import pdb; pdb.set_trace()
+    return redirect(url_for('index'))
+
+app.run(debug=True, host='0.0.0.0', port= 8000)
