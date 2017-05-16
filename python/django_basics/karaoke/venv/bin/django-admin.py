c47cc49f9bc3d3d543a295f1bb37cbe172d3f860
Brigette Eckert <Brigette.Eckert@live.com>
2017-05-16 15:05:14 -0700

    updated gitignore

--- python/django_basics/karaoke/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/cygdrive/c/Users/Brigette/Coding/treehouse/python/django_basics/karaoke/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
